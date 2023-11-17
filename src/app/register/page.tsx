b16b059f7fd6bbf82ba7385cf78cf0217e76b13b
Shakeel Haider <shkhaider2015@gmail.com>
2023-11-17 14:35:29 +0500

    just add react-hook-form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,6 +3,9 @@ export default async function Register() {
     return (
       <main className="">
         <h1>Register</h1>
+        <form action="">
+          <input type="text" name="" id="" />
+        </form>
       </main>
     );
   }
