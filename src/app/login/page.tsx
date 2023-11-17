043ef6c36e641741a6a0162dacabc5d34cc900ad
Shakeel Haider <shkhaider2015@gmail.com>
2023-11-17 12:32:50 +0500

    add nextauth

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default async function Login() {
+    // const session = await getServerSession();
+    return (
+      <main className="">
+        <h1>Login</h1>
+      </main>
+    );
+  }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,8 @@
+export default async function Register() {
+    // const session = await getServerSession();
+    return (
+      <main className="">
+        <h1>Register</h1>
+      </main>
+    );
+  }
